b8474bd796b63adafa7d13e717d5fbec9593e738
ToPin060 <tristan.pin81@gmail.com>
2024-08-07 21:10:06 +0200

    Feat: Implement a simple header
    Note: Logo is a substitue, I'm waiting for the customer's one.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils";
+import { Header } from "@/components/common/Header";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
       <body
         className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}
       >
+        <Header />
         {children}
       </body>
     </html>
